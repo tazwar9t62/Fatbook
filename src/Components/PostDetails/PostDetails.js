755aa848e70b13a3ffc685fa8bf93d8333aec5e5
Tazwar <tazwar9t6@gmail.com>
2020-09-05 22:55:22 +0600

    90% done and tired

--- src/Components/PostDetails/PostDetails.js
+++ src/Components/PostDetails/PostDetails.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Axios from "axios";
 import Header from "../Header/Header";
-import { Box, Avatar } from "@material-ui/core";
+import { Box, Avatar, TextField } from "@material-ui/core";
 
 const PostDetails = () => {
   let { postID } = useParams();
@@ -25,26 +25,42 @@ const PostDetails = () => {
   return (
     <div>
       <Header></Header>
-      <Box mx={20} my={3} p={3} boxShadow={2}>
+      <Box mx={20} p={3} boxShadow={2}>
         <h1>{title}</h1>
         <pre>{body}</pre>
         <h3 color="text.warning.main">Comments:</h3>
         <ul>
           {comments.map((singleComment) => (
-            <Box>
-              <Avatar
-                alt="Remy Sharp"
-                src={`https://graph.facebook.com/v2.5/${
-                  postID + Math.floor(Math.random() * 1000)
-                }/picture?height=200&height=200`}
-              />
-
+            <Box
+              display="flex"
+              flexDirection="row"
+              p={1}
+              m={1}
+              bgcolor="background.paper"
+            >
+              {" "}
+              <Box mr={3}>
+                <Avatar
+                  alt="Profile Picture"
+                  src={`https://graph.facebook.com/v2.5/${
+                    postID + Math.floor(Math.random() * 1000)
+                  }/picture?height=200&height=200`}
+                />
+              </Box>
               {singleComment.name}
               <br></br>
               {singleComment.body}
             </Box>
           ))}
         </ul>
+
+        <form noValidate autoComplete="off">
+          <TextField
+            id="outlined-basic"
+            label="Your Comment"
+            variant="outlined"
+          />
+        </form>
       </Box>
     </div>
   );
